b895f8f95ed899e1bacfd0a49fcc54f9235a34d4
VadimTalimonchik <tvadmins@gmail.com>
2023-01-16 16:49:39 +0300

    Перебор слов, обход директории

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -84,31 +84,109 @@
 // }
 // Console.WriteLine(FactorialRec(10));
 
-// Задача 5
-// Возведение числа а в натуральную степень n
-// Итеративный подход
-int PowerFor(int a, int n)
-{
-    int result = 1;
-    for(int i = 1; i <= n; i++) result *= a;
-    return result;
-}
-Console.WriteLine(PowerFor(2, 10));
+//===========================
+// // Задача 5
+// // Возведение числа а в натуральную степень n
+// // Итеративный подход
+// int PowerFor(int a, int n)
+// {
+//     int result = 1;
+//     for(int i = 1; i <= n; i++) result *= a;
+//     return result;
+// }
+// Console.WriteLine(PowerFor(2, 10));
 
-// Итеративный подход
-int PowerRec(int a, int n)
-{
-    // return n == 0 ? 1 : PowerRec(a, n - 1) * a;
-    if(n == 0) return 1;
-    else return PowerRec(a, n - 1) * a;
-}
-Console.WriteLine(PowerRec(2, 10));
+// // Итеративный подход
+// int PowerRec(int a, int n)
+// {
+//     // return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//     if(n == 0) return 1;
+//     else return PowerRec(a, n - 1) * a;
+// }
+// Console.WriteLine(PowerRec(2, 10));
 
-// Применение формул для возведения в степень
-int PowerRecMath(int a, int n)
+// // Применение формул для возведения в стwwепень
+// int PowerRecMath(int a, int n)
+// {
+//     if(n == 0) return 1;
+//     else if(n % 2 == 0) return PowerRecMath(a * a, n / 2);
+//     else return PowerRecMath(a, n - 1) * a;
+// }
+// Console.WriteLine(PowerRecMath(2, 10));
+
+//==================
+// // Задача 6
+// // В некотором машинном алфавите имеются 4 буквы "а", "и", "с" и "в".
+// // Покажите все слова, состоящие из Т букв, которые можно построить из букв этого алфавита.
+// // Итеративный подход
+// char[] s = {'а', 'и', 'с', 'в'};
+// int count = s.Length;
+// int n = 1;
+// for(int i = 0; i < count; i++)
+// {
+//     for(int j = 0; j < count; j++)
+//     {
+//         for(int k = 0; k < count; k++)
+//         {
+//             for(int l = 0; l < count; l++)
+//             {
+//                 for(int m = 0; m < count; m++)
+//                 {
+//                     Console.WriteLine($"{n++, -5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// // Рекурсивный подход
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if(length == word.Length)
+//     {
+//         Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for(int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length + 1);
+//     }
+// }
+// FindWords("аисв", new char[5]);
+
+//========================
+// // Задача 7
+// // Алгоритм обхода директории
+// // Итеративный подход
+// string path = "D:/ПРОГРАММИРОВАНИЕ/GEEK_BRAINS/РАЗРАБОТЧИК GeekBrains/EDUCATION/C#/lectures";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for(int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+// Рекурсивный подход
+void CatalogInfo(string path, string indent = "")
 {
-    if(n == 0) return 1;
-    else if(n % 2 == 0) return PowerRecMath(a * a, n / 2);
-    else return PowerRecMath(a, n - 1) * a;
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for(int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for(int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
 }
-Console.WriteLine(PowerRecMath(2, 10));
+string path = @"D:\ПРОГРАММИРОВАНИЕ\GEEK_BRAINS\РАЗРАБОТЧИК GeekBrains\EDUCATION\C#\lectures";
+CatalogInfo(path);
