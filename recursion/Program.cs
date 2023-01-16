3492ff349425cc7e8292df06baa07d134af6aca1
VadimTalimonchik <tvadmins@gmail.com>
2023-01-16 17:19:06 +0300

    Игра в пирамидки

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -169,24 +169,35 @@
 //     System.Console.WriteLine(fi[i].Name);
 // }
 
-// Рекурсивный подход
-void CatalogInfo(string path, string indent = "")
+// // Рекурсивный подход
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo catalog = new DirectoryInfo(path);
+
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for(int i = 0; i < catalogs.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + "  ");
+//     }
+
+//     FileInfo[] files = catalog.GetFiles();
+
+//     for(int i = 0; i < files.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{files[i].Name}");
+//     }
+// }
+// string path = @"D:\ПРОГРАММИРОВАНИЕ\GEEK_BRAINS\РАЗРАБОТЧИК GeekBrains\EDUCATION\C#\lectures";
+// CatalogInfo(path);
+
+//=================================
+// Задача 8
+// Игра в пирамидки (рекурсия)
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
 {
-    DirectoryInfo catalog = new DirectoryInfo(path);
-
-    DirectoryInfo[] catalogs = catalog.GetDirectories();
-    for(int i = 0; i < catalogs.Length; i++)
-    {
-        Console.WriteLine($"{indent}{catalogs[i].Name}");
-        CatalogInfo(catalogs[i].FullName, indent + "  ");
-    }
-
-    FileInfo[] files = catalog.GetFiles();
-
-    for(int i = 0; i < files.Length; i++)
-    {
-        Console.WriteLine($"{indent}{files[i].Name}");
-    }
+    if(count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if(count > 1) Towers(some, on, with, count - 1);
 }
-string path = @"D:\ПРОГРАММИРОВАНИЕ\GEEK_BRAINS\РАЗРАБОТЧИК GeekBrains\EDUCATION\C#\lectures";
-CatalogInfo(path);
+Towers();
